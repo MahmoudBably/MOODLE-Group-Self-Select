0f2e62d63fadce2259652ef8a18ddb27b1b2cb37
MahmoudBably <38445046+MahmoudBably@users.noreply.github.com>
2018-04-18 11:03:05 +0200

    Add files via upload

---
+++ groupselect/UserHelper.php
@@ -0,0 +1,351 @@
+<?php
+ echo'<HTML>
+
+<HEAD>
+
+    <TITLE>User Helper</TITLE>
+
+</HEAD>
+
+<BODY BGCOLOR="#FFFFFF">
+
+
+
+   
+    
+
+    <center><font size="6"><H1>Group Self-Selection User Helper</H1><img src="../groupselect/UserHelper/Logo.png" alt="Icon" height="50" width="80" style="margin-top:-105px;margin-right:-1020px"></font></center>
+
+
+    <br />
+    <br />
+    <br />
+    <br />
+    <br /> 
+    <h2>
+<a href="http://localhost/course">Back to courses</a>
+    </form></h2>
+    <hr />
+    <style>
+        div.box {
+            background-color: #F8F8F8;
+            border: 1px solid black;
+        }
+    </style>
+
+
+    <H1><u>Teachers Guide:-</u></H1>
+    <h2>This section will cover the following Topics:</h2>
+    <div class="box">
+        <center>a)Create a group self-select activity.</center><br /><br />
+        
+        <center>b)Create a group.</center><br /><br />
+        <center>c)Assign a Supervisor.</center><br /><br />
+        <center>d)Export group data file (CSV).</center><br /><br />
+        <center>e)Add Students to groups.</center>
+    </div>
+
+    <br /><br /><br /><br /><br />
+
+    <h2>a)Create a group self-select activity:</h2>
+
+
+    1)To create a group self-select, first navigate to a course that you have created.<br><br>
+    <CENTER><img src="../groupselect/UserHelper/How to Add Groupselect/1.png" height="500" width="900"></CENTER>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    2)Click on the settings icon.<br />
+    <br />
+    <CENTER><img src="../groupselect/UserHelper/How to Add Groupselect/2.png" height="500" width="900"></CENTER>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    3)Click on "Turn Editting On".
+    <br />
+    <br />
+    <CENTER><img src="../groupselect/UserHelper/How to Add Groupselect/3.png" height="500" width="900"></CENTER>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    4)Click on "Add an activity or resource".
+    <br />
+    <br />
+    <CENTER><img src="../groupselect/UserHelper/How to Add Groupselect/4.png" height="500" width="900"></CENTER>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    5)Choose the "Group self-selection" from the list then click on the add button.
+    <br />
+    <br />
+    <CENTER><img src="../groupselect/UserHelper/How to Add Groupselect/5.png" height="500" width="900"></CENTER>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    6)Write a name and a description of your own preference, you can also set the desired settings as you scroll down.
+    <br />
+    <br />
+    <CENTER><img src="../groupselect/UserHelper/How to Add Groupselect/6.png" height="500" width="900"></CENTER>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    7)Click on "Save and return to course".
+    <br />
+    <br />
+    <CENTER><img src="../groupselect/UserHelper/How to Add Groupselect/7.png" height="500" width="900"></CENTER>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    8)the group self-select activity "Group Select 2" was created successfully.
+    <br />
+    <br />
+    <CENTER><img src="../groupselect/UserHelper/How to Add Groupselect/8.png" height="500" width="900"></CENTER>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+
+
+    <h2>b)Create a group:</h2>
+
+    1)To create a group after you created a group self-select, click on the "Create a new group" icon.
+    <CENTER><img src="../groupselect/UserHelper/Create Group/1.png" height="500" width="900"></CENTER>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    2)Write a name for the group and its description, then click on "Createa a new group".<br />
+    <br />
+    <CENTER><img src="../groupselect/UserHelper/Create Group/2.png" height="500" width="900"></CENTER>    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    3)The Group is created successfully.
+    <br />
+    <br />
+    <CENTER><img src="../groupselect/UserHelper/Create Group/3.png" height="500" width="900"></CENTER>    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    4)Click on "Add an activity or resource".
+    <br />
+    <br />
+    <CENTER><img src="../groupselect/UserHelper/Create Group/1.png" height="500" width="900"></CENTER>    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+
+    <h2>c)Assign a Supervisor:</h2>
+
+    1)Click on "Assign supervisors to groups". Pressing the assignment button divides the groups randomly and evenly to supervisors. Assignment is reversible and can be redone. <br /><br />
+    <CENTER><img src="../groupselect/UserHelper/Assign Supervisor/1.png" height="500" width="900"></CENTER>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    2)Supervisors are assigned to corresponding groups randomly.<br />
+    <br />
+    <CENTER><img src="../groupselect/UserHelper/Assign Supervisor/2.png" height="500" width="900"></CENTER>    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+
+    <h2>d)Export group data file (CSV):</h2>
+
+    1)Click on "Create a download link for group data file(CSV)".This will create a link for you to download the CSV file <br /><br />
+    <CENTER><img src="../groupselect/UserHelper/CSV Export/1.png" height="500" width="900"></CENTER>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    2)Click on "Download CSV-file".<br />
+    <br />
+    <CENTER><img src="../groupselect/UserHelper/CSV Export/2.png" height="500" width="900"></CENTER>    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    3)The file is downloaded successfully. When it is opened using Excel Sheet, it is shown as the following picture.
+    <br />
+    <br />
+    <CENTER><img src="../groupselect/UserHelper/CSV Export/3.png" height="500" width="900"></CENTER>    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    
+    <h2>e)Add Students to groups:</h2>
+
+    1)Click on the Select Student dropdown menu.<br /><br />
+    <CENTER><img src="../groupselect/UserHelper/Add Students/1.png" height="500" width="900"></CENTER>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    2)Select a student from the menu, then click on "Add Student" button.<br />
+    <br />
+    <CENTER><img src="../groupselect/UserHelper/Add Students/2.png" height="500" width="900"></CENTER>    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    3)The Student was added successfully.
+    <br />
+    <br />
+    <CENTER><img src="../groupselect/UserHelper/Add Students/3.png" height="500" width="900"></CENTER>    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <hr />
+
+    <H1><u>Students Guide:-</u></H1>
+    <h2>This section will cover the following Topics</h2>
+    <div class="box">
+       <center> a)Create a group.</center><br /><br />
+       <center> b)Join a group.<center>
+
+    </div>
+    <br /><br /><br /><br /><br />
+    <h2> a)Create a group.</h2>
+
+    1)Click on "Create a new group".
+    <br />
+    <br />
+    <CENTER><img src="../groupselect/UserHelper/Student Create Group/1.png" height="500" width="900"></CENTER>    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    2)Write a name for the group, you can also choose to make the group accessible only through a password, then click on "Create a new group".
+    <br />
+    <br />
+    <CENTER><img src="../groupselect/UserHelper/Student Create Group/2.png" height="500" width="900"></CENTER>    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <hr />
+    1)The group is created successfully and is password-protected.<br />
+    <br />
+    <CENTER><img src="../groupselect/UserHelper/Student Create Group/3.png" height="500" width="900"></CENTER>    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+
+   <h2>b)Join a group</h2> 
+    <br /><br />
+    1)Simply click on "Become member of" button next to any group that you want to join.<br />
+    <br />
+    <CENTER><img src="../groupselect/UserHelper/Student Join Group/1.png" height="500" width="900"></CENTER>    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    2)You are now a member of the "Task 1" group.
+    <br />
+    <br />
+    <CENTER><img src="../groupselect/UserHelper/Student Join Group/2.png" height="500" width="900"></CENTER>    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <hr />
+
+
+
+
+
+</BODY>
+
+
+</HTML>';
